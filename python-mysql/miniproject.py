75732edaecc46bdd07d8777315696f246e345466
Thalles Rodrigues <thalles07rodri@gmail.com>
2022-05-11 14:22:12 -0300

    Sincronização de dois docker

---
+++ python-mysql/miniproject.py
@@ -0,0 +1,16 @@
+# Acessar o banco de dados via python
+
+import mysql.connector
+# Pode acessar o host através do nome do container em vez do IP
+db = mysql.connector.connect(host='mysql', user='root', password='root', port=3306) #conexão com o BD
+
+my_cursor=db.cursor()
+my_cursor.execute('DROP database IF EXISTS brq')
+my_cursor.execute('CREATE DATABASE IF NOT EXISTS brq_python')
+my_cursor.execute('use brq_python')
+my_cursor.execute('CREATE TABLE IF NOT EXISTS feras_brq( nome varchar(255), email varchar(255) )')
+my_cursor.execute('INSERT INTO feras_brq (nome,email) VALUES ("Joao", "j@j.com" )')
+
+db.commit()
+
+my_cursor.close()
